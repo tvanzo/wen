9baa31bd5039b4173177366cd9c41a71f971119c
EC2 Default User <ec2-user@ip-172-31-16-214.us-east-2.compute.internal>
2018-01-31 06:03:43 +0000

    adding google login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,3 @@
-OmniAuth.config.logger = Rails.logger
-
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, 'YOUR-APP-ID-HERE', 'YOUR-APP-SECRET-HERE'
+  provider :google_oauth2, "89828774313-tcd50q6jr3oq13fbk5r60132vvuj75l8.apps.googleusercontent.com", "8GW9fg7ZWe1gdDySB1rv6W0c"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180128103036) do
+ActiveRecord::Schema.define(version: 20180131054125) do
 
   create_table "comments", force: :cascade do |t|
     t.string "username"
@@ -29,8 +29,12 @@ ActiveRecord::Schema.define(version: 20180128103036) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "provider"
+    t.string "uid"
     t.string "email"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "picture"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
